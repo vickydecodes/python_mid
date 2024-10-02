c45fadec2a1d3d6f351b1f152cdb4a64d93e63b9
vicky <144013983+vickydecodes@users.noreply.github.com>
2024-10-02 22:40:08 +0530

    Add files via upload

---
+++ p15.py
@@ -0,0 +1,22 @@
+file = open('D:/Documents/semesterpy/txt3.txt', 'r')
+
+wordcount = {}
+
+for word in file.read().split():
+    if word not in wordcount:
+        wordcount[word] = 1
+    else:
+        wordcount[word] += 1
+
+print(wordcount)
+
+maxno = 5
+
+for key in wordcount.keys():
+    if wordcount[key] > maxno:
+        maxno = wordcount[key]
+
+
+for key in wordcount.keys():
+    if wordcount[key] == maxno:
+        print(key)
