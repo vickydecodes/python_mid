265fc6ddca374a2d607ac4cdd18bfd15e44d984f
vicky <144013983+vickydecodes@users.noreply.github.com>
2024-10-03 00:17:39 +0530

    Update p18.py

--- p18.py
+++ p18.py
@@ -5,4 +5,4 @@ str = input('Enter the string: ')
 if str == str[::-1]:
     print(f'{str} is a palindrome')
 else:
-        print(f'{str} is not a palindrome')
+    print(f'{str} is not a palindrome')
