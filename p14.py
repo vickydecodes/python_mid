0d41c357e9ca792d9cae784c61921e284f46947c
vicky <144013983+vickydecodes@users.noreply.github.com>
2024-10-02 16:31:03 +0530

    semester update

---
+++ p14.py
@@ -0,0 +1,19 @@
+list = []
+length = int(input('Enter the length: '))
+for i in range(length):
+    a = int(input('Enter the number to append: '))
+    list.append(a)
+    
+print(list)
+
+def selection_sort(list):
+    for i in range(len(list)):
+      smallest = i
+      for j in range(i+1, len(list)):
+        if list[j] < list[smallest]:
+            smallest = j
+            list[i], list[smallest] = list[smallest],list[i]
+    return list
+
+print(selection_sort(list))
+   
